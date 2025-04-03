9f3cc5a80ad47d840e39297424648308c1698548
Manisha Rana <manisharana02005@gmail.com>
2025-04-03 11:47:34 +0530

    defined the schema of postModel

--- models/postModel.js
+++ models/postModel.js
@@ -1,1 +1,33 @@
 
+
+//import instance of mongoose
+const mongoose= require("mongoose");
+
+
+//route handler
+const postSchema = new mongoose.Schema({
+
+    title:{
+        type: String,
+        required: true,
+    },
+
+    body:{
+        type: String,
+        required: true,
+    },
+
+    likes:{
+        type: mongoose.Schema.Types.ObjectId, //id
+        ref:"Like",
+    },
+
+    comment:{
+        type: mongoose.Schema.Types.ObjectId, //id
+        ref:"Comment",
+    }
+    
+});
+
+//export 
+module.exports=mongoose.model("Post", postSchema);
